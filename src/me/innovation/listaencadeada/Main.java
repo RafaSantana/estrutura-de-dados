f97b5f72499598bcb1ee8c79f1591827434e7def
RafaSantana <rafaelsantana.tech@gmail.com>
2023-11-04 12:52:38 -0300

    Utilização da classe ListaEncadeada

--- src/me/innovation/listaencadeada/Main.java
+++ src/me/innovation/listaencadeada/Main.java
@@ -2,7 +2,21 @@ package me.innovation.listaencadeada;
 
 public class Main {
     public static void main(String[] args) {
+        Listaencadeada<String> minhaListaEncadeada = new Listaencadeada<>();
 
+        minhaListaEncadeada.add("teste1");
+        minhaListaEncadeada.add("teste2");
+        minhaListaEncadeada.add("teste3");
+        minhaListaEncadeada.add("teste4");
+
+        System.out.println(minhaListaEncadeada.get(0));
+        System.out.println(minhaListaEncadeada.get(1));
+        System.out.println(minhaListaEncadeada.get(2));
+        System.out.println(minhaListaEncadeada.get(3));
+
+        System.out.println(minhaListaEncadeada);
+
+        System.out.println(minhaListaEncadeada.remove(3));
+        System.out.println(minhaListaEncadeada);
     }
-
 }
