d79e4ab02a52211a090c523cea26270615a9f7c8
RafaSantana <rafaelsantana.tech@gmail.com>
2023-11-08 14:54:11 -0300

    Implementado método add na classe ListaDuplamenteEncadeada.

--- src/me/innovation/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
+++ src/me/innovation/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
@@ -2,14 +2,14 @@ package me.innovation.listaduplamenteencadeada;
 
 public class ListaDuplamenteEncadeada<T> {
 
-    private NoDuplo<T> primeNoDuplo;
-    private NoDuplo<T> ultimoNoDuplo;
+    private NoDuplo<T> refPrimeiroNo;
+    private NoDuplo<T> refUltimoNo;
 
     private int tamanhoLista;
 
     public ListaDuplamenteEncadeada() {
-        this.primeNoDuplo = null;
-        this.ultimoNoDuplo = null;
+        this.refPrimeiroNo = null;
+        this.refUltimoNo = null;
         this.tamanhoLista = 0;
     }
 
@@ -17,11 +17,25 @@ public class ListaDuplamenteEncadeada<T> {
         return this.getNo(index).getConteudo();
     }
 
+    public void add(T conteudo) {
+        NoDuplo<T> novoNo = new NoDuplo<>(conteudo);
+        novoNo.setRefNoAnterior(refUltimoNo);
+        novoNo.setRefNoPosterior(null);
+        if (refPrimeiroNo == null) {
+            refPrimeiroNo = novoNo;
+        }
+        if (refUltimoNo != null) {
+            refUltimoNo.setRefNoPosterior(novoNo);
+        }
+        refUltimoNo = novoNo;
+        this.tamanhoLista++;
+    }
+
     private NoDuplo<T> getNo(int index) {
-        NoDuplo<T> noAuxiliar = primeNoDuplo;
+        NoDuplo<T> noAuxiliar = refPrimeiroNo;
 
         for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
-            noAuxiliar = noAuxiliar.getNoProximo();
+            noAuxiliar = noAuxiliar.getRefNoPosterior();
         }
         return noAuxiliar;
     }
