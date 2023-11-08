e3871604ad3704b1092a65f7392b24e1ba3c5eb1
RafaSantana <rafaelsantana.tech@gmail.com>
2023-11-08 09:37:32 -0300

    refatoração de variável e implementação dos métodos getNo e get na classe ListaDuplamenteEncadeada.

--- src/me/innovation/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
+++ src/me/innovation/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
@@ -3,14 +3,27 @@ package me.innovation.listaduplamenteencadeada;
 public class ListaDuplamenteEncadeada<T> {
 
     private NoDuplo<T> primeNoDuplo;
-    private NoDuplo<T> ultimNoDuplo;
+    private NoDuplo<T> ultimoNoDuplo;
 
     private int tamanhoLista;
 
     public ListaDuplamenteEncadeada() {
         this.primeNoDuplo = null;
-        this.ultimNoDuplo = null;
+        this.ultimoNoDuplo = null;
         this.tamanhoLista = 0;
+    }
+
+    public T get(int index) {
+        return this.getNo(index).getConteudo();
+    }
+
+    private NoDuplo<T> getNo(int index) {
+        NoDuplo<T> noAuxiliar = primeNoDuplo;
+
+        for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
     }
 
     public int size() {
